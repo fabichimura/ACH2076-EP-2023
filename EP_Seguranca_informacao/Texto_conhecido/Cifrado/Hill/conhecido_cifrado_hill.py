04b431ae3ecbc11efb6ec2b10c6f8a83f077a7dd
fabichimura <fabichimura@usp.br>
2023-06-26 22:06:10 -0300

    refatora parte da encriptacao

--- EP_Seguranca_informacao/Texto_conhecido/Cifrado/Hill/conhecido_cifrado_hill.py
+++ EP_Seguranca_informacao/Texto_conhecido/Cifrado/Hill/conhecido_cifrado_hill.py
@@ -5,9 +5,22 @@ import unicodedata
 import numpy as np
 import sympy as sp
 
-def is_invertible(matrix):
-    det = np.linalg.det(matrix)
+def eh_invertivel(matriz):
+    det = np.linalg.det(matriz)
     return det != 0
+
+def inverso_modulo26(matriz):
+    # Calcula a matriz adjunta
+    matriz_adjunta = np.round(np.linalg.inv(matriz) * np.linalg.det(matriz))
+    # Calcula o inverso multiplicativo do determinante em módulo 26
+    det_inv_mod_26 = pow(int(np.linalg.det(matriz)), -1, 26)
+    # Calcula a matriz inversa em módulo 26
+    inverso_mod_26 = (matriz_adjunta * det_inv_mod_26) % 26
+    return inverso_mod_26
+
+def converte_matriz_to_int(matriz):
+    matriz_convertida = np.round(matriz).astype(int)
+    return matriz_convertida
 
 def remover_acentos(texto):
     texto_sem_acentos = ''.join(
@@ -31,11 +44,11 @@ def substituir_por_letra(numero):
         return ""
 
 
-with open('Grupo07_texto_cifrado.txt','r') as arquivo:
+with open('EP_Seguranca_informacao\Texto_conhecido\Cifrado\Hill\Grupo07_texto_cifrado.txt','r') as arquivo:
     texto_cifrado = arquivo.read()
     tam_texto_cifrado = len(texto_cifrado)
 
-with open('policarpo_quaresma.txt', 'r', encoding='utf-8') as arquivo:
+with open('EP_Seguranca_informacao\Texto_conhecido\Cifrado\Hill\policarpo_quaresma.txt', 'r', encoding='utf-8') as arquivo:
     policarpo_quaresma = arquivo.read()
 
     # transforma as letras em minúsculas
@@ -47,7 +60,7 @@ with open('policarpo_quaresma.txt', 'r', encoding='utf-8') as arquivo:
     # remove todos os caracteres que não são letras
     policarpo_quaresma = re.sub(r'[^a-z]', '', policarpo_quaresma)
 
-with open('policarpo_analise.txt','w', encoding='utf-8') as arquivo_saida:
+with open('EP_Seguranca_informacao\Texto_conhecido\Cifrado\Hill\policarpo_analise.txt','w', encoding='utf-8') as arquivo_saida:
     arquivo_saida.write(policarpo_quaresma)
 
 # pelo GeraEP1.py -> a matriz para a encriptação é 2 x 2)
@@ -57,8 +70,9 @@ matrizes = [list(zip(matriz[:2], matriz[2:])) for matriz in matrizes]
 matrizes_invertiveis = []
 
 for matriz in matrizes:
-    if(is_invertible(matriz)):
+    if(eh_invertivel(matriz)):
         matrizes_invertiveis.append(matriz)
+
 
 # decripta e verifica se esta no texto
 indice = 0
@@ -82,32 +96,27 @@ for matriz in matrizes_invertiveis:
 
         print('Este é o trecho decriptado:', texto_decriptado)
         print('Esta é a matriz que gerou', matriz)
+        break
     else:
         texto_decriptado = ''
-    
 
 
 # encripta novamente para conferir
-'''
-trecho_decriptado1 =  'oporemtaocoherentecomellemesmotaodeaccordocomasubstanciadavidaqueellemesmofabricaraqueselimitouasorr'
-# calculado na mao
-matriz_chave = [[7, 0], [4, 3]]
-numeros_encriptado = substituir_por_posicao(trecho_decriptado1)
-
-texto_decriptado = ''
+matriz_inversa_chave = [[15,0], [6,9]]
+matriz_chave = converte_matriz_to_int(inverso_modulo26(matriz_inversa_chave)) 
+numeros_encriptado = substituir_por_posicao(texto_decriptado)
+texto_encriptado = ''
 for i in range(0, 100, 2):
     if (i == 0):
         matriz_bloco = np.array([[numeros_encriptado[0], numeros_encriptado[1]]])
     else:
         matriz_bloco = np.array([[numeros_encriptado[i], numeros_encriptado[i + 1]]])
-    #print(matriz_bloco)
     produto = np.dot(matriz_bloco, matriz_chave)
     produto = produto[0].tolist()
     produto_modulo = list(map(lambda x: x % 26, produto))
     primeiro_caract = substituir_por_letra(produto_modulo[0])
     segundo_caract = substituir_por_letra(produto_modulo[1])
-    texto_decriptado = texto_decriptado + primeiro_caract + segundo_caract
-print(texto_decriptado)
-print(texto_decriptado == texto_cifrado)
-print(is_invertible(matriz_chave))
-'''
+    texto_encriptado = texto_encriptado + primeiro_caract + segundo_caract
+print(texto_encriptado)
+print(texto_encriptado == texto_cifrado)
+print(eh_invertivel(matriz_chave))
